a26ee95fd7067b18b1ca07a7ed3c72598ebb2604
cmontella <cmontella@live.com>
2021-04-26 19:27:04 -0400

    Add #system/exit[]

---
+++ src/exit.rs
@@ -0,0 +1,58 @@
+extern crate crossbeam_channel;
+use mech_core::{hash_string, TableIndex, Table, Value, ValueType, ValueMethods, Transaction, Change, TableId, Register};
+use mech_utilities::{Machine, MachineRegistrar, RunLoopMessage};
+//use std::sync::mpsc::{self, Sender};
+use std::thread::{self};
+use crossbeam_channel::Sender;
+use std::collections::HashMap;
+use std::io;
+use std::io::prelude::*;
+
+lazy_static! {
+  static ref SYSTEM_EXIT: u64 = hash_string("system/exit");
+}
+
+export_machine!(system_exit, system_exit_reg);
+
+extern "C" fn system_exit_reg(registrar: &mut dyn MachineRegistrar, outgoing: Sender<RunLoopMessage>) -> Vec<Change> {
+  registrar.register_machine(Box::new(Exit{outgoing}));
+  vec![
+    Change::NewTable{table_id: *SYSTEM_EXIT, rows: 0, columns: 0},
+  ]
+}
+
+#[derive(Debug)]
+pub struct Exit {
+  outgoing: Sender<RunLoopMessage>,
+}
+
+impl Machine for Exit {
+
+  fn name(&self) -> String {
+    "system/exit".to_string()
+  }
+
+  fn id(&self) -> u64 {
+    Register{table_id: TableId::Global(*SYSTEM_EXIT), row: TableIndex::All, column: TableIndex::All}.hash()
+  }
+
+  fn on_change(&mut self, table: &Table) -> Result<(), String> {
+    let (value, _) = table.get_unchecked(1,1);
+    match value.value_type() {
+      ValueType::Quantity => {
+        let exit_code = value.as_i64().unwrap() as i32;
+        self.outgoing.send(RunLoopMessage::Exit(exit_code));
+      }
+      ValueType::Boolean => {
+        let exit_code = value.as_bool().unwrap() as i32;
+        self.outgoing.send(RunLoopMessage::Exit(exit_code));
+      }
+      ValueType::NumberLiteral => {
+        // TODO print number literals
+        self.outgoing.send(RunLoopMessage::Exit(0));
+      }
+      _ => {self.outgoing.send(RunLoopMessage::Exit(0));}
+    }
+    Ok(())
+  }
+}
