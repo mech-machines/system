c132f3d7782bb4f91876b38bd7e24d142c589981
cmontella <cmontella@live.com>
2023-01-04 16:49:27 -0500

    remove unused imports

--- src/exit.rs
+++ src/exit.rs
@@ -1,12 +1,7 @@
 extern crate crossbeam_channel;
 use mech_core::*;
 use mech_utilities::*;
-//use std::sync::mpsc::{self, Sender};
-use std::thread::{self};
 use crossbeam_channel::Sender;
-use std::collections::HashMap;
-use std::io;
-use std::io::prelude::*;
 
 lazy_static! {
   static ref SYSTEM_EXIT: u64 = hash_str("system/exit");
